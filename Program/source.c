8d68345164a0d47ad326bcf32ab5a38b3dda0729
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-04-16 19:45:18 +0900

    (2026.4.16) 시간 시스템

--- Program/source.c
+++ Program/source.c
@@ -1,4 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+
+#define SIZE 10000
+
+void load(const char * file_name)
+{
+	FILE * file = fopen(file_name, "r");
+
+	char buffer[SIZE] = { 0, };
+
+	// 첫 번째 매개변수 : 읽을 데이터를 저장할 메모리 버퍼의 포인터 변수
+	// 두 번째 매개변수 : 각 데이터 항목의 크기
+	// 세 번째 매개변수 : 데이터를 읽어올 항목의 수
+	// 네 번째 매개변수 : 데이터를 읽어올 파일의 포인터 변수
+
+	fread(buffer, sizeof(char), SIZE, file);
+
+	printf("%s", buffer);
+
+	fclose(file);
+}
 
 int main()
 {
@@ -14,6 +36,8 @@ int main()
 	fputs("Defense : \n", file);
 
 	fclose(file);
+
+	load("Pokemon.txt");
 
 #pragma endregion
 
